1cebe7292a31e8770a74ca9880910f80e7129dcd
Tomas <tmgarcia@miuandes.cl>
2019-04-12 14:30:57 -0400

    user ready

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = [Registered_user.new(mail: "t@us.com", username: "tomgar", password: "1234"),
+Registered_user.new(mail: "m@us.com", username: "resnak", password: "1234"), 
+Registered_user.new(mail: "h@us.com", username: "hugo", password: "1234"),
+Registered_user.new(mail: "n@us.com", username: "aspee", password: "1234"),
+Registered_user.new(mail: "m@us.com", username: "costa", password: "1234")]
+
+for u in users do 
+	u.save!
+end
+
+admins = [Admin.new(mail: "admin1@ad.com", password: "123", username: "admin1", geofence: "Las condes"),
+Admin.new(mail: "admin2@ad.com", password: "123", username: "admin2", geofence: "Lo barnechea"),
+Admin.new(mail: "admin3@ad.com", password: "123", username: "admin2", geofence: "Vitacura")]
+
+for a in admins do 
+	a.save!
+end
+
+sys = System_admin.new(mail: "system@sy.com", username: "super1", password: "123")
+
+sys.save!
+
+
+
