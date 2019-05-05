9c424aaf41d06df94e95ed5e2c269877c364029d
resnakk <masanchez3@miuandes.cl>
2019-05-05 02:10:13 -0400

    f

--- project/app/views/registred_users/_registred_user.json.jbuilder
+++ project/app/views/registred_users/_registred_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! registred_user, :id, :mail, :password, :username, :created_at, :updated_at
+json.extract! registred_user, :id, :mail, :password, :username,:name,:last_name,:phone,:city,:country, :created_at, :updated_at
 json.url registred_user_url(registred_user, format: :json)
--- project/db/migrate/20190412173359_create_registred_users.rb
+++ project/db/migrate/20190412173359_create_registred_users.rb
@@ -1,12 +1,14 @@
 	class CreateRegistredUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :registred_users do |t|
+      t.string :name
+      t.string :last_name
       t.string :mail
       t.string :password
       t.string :username
       t.integer :phone
       t.string :city
-      t.string :country
+      t.string :country	
 
       t.timestamps
     end
