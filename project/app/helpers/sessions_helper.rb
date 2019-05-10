eaddc0ed68f04cbb513d3d96314318512da9574e
Tomas <tmgarcia@miuandes.cl>
2019-05-10 03:52:03 -0400

    pati mi cola

--- project/app/helpers/sessions_helper.rb
+++ project/app/helpers/sessions_helper.rb
@@ -6,5 +6,19 @@ module SessionsHelper
 		session.delete(:registred_user_id)
 		@current_registred_user = nil
 	end
+	def log_inA(admin)
+		session[:admin_id] = admin.id
+	end
+	def log_outA
+		session.delete(:admin_id)
+		@current_admin = nil
+	end
+	def log_inS(system_admin)
+		session[:system_admin_id] = system_admin.id
+	end
+	def log_outS
+		session.delete(:system_admin_id)
+		@current_system_admin = nil
+	end
 end
 	
