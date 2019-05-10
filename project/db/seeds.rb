250e63321ff80d3ba5900cfc25b879521fb95a38
Tomas <tmgarcia@miuandes.cl>
2019-05-10 04:23:28 -0400

    pati mi cola

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -5,25 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-users = [RegistredUser.new(mail: "t@us.com", username: "tomgar", password: "1234"),
-RegistredUser.new(mail: "m@us.com", username: "resnak", password: "1234"), 
-RegistredUser.new(mail: "h@us.com", username: "hugo", password: "1234"),
-RegistredUser.new(mail: "n@us.com", username: "aspee", password: "1234"),
-RegistredUser.new(mail: "m@us.com", username: "costa", password: "1234")]
+users = [RegistredUser.new(mail: "tom@us.com", username: "tomgar12", password: "Qwer1234", name: "tom", last_name: "gar"),
+RegistredUser.new(mail: "mau@us.com", username: "resnak12", password: "Qwer1234",name: "mauro", last_name: "sanchez"), 
+RegistredUser.new(mail: "hug@us.com", username: "hugo12", password: "Qwer1234",name: "hugo", last_name: "dela"),
+RegistredUser.new(mail: "nic@us.com", username: "aspee12", password: "Qwer1234",name: "nico", last_name: "gor"),
+RegistredUser.new(mail: "mar@us.com", username: "costa12", password: "Qwer1234"),name: "martin", last_name: "concha"]
 
 for u in users do 
 	u.save!
 end
 
-admins = [Admin.new(mail: "admin1@ad.com", password: "123", username: "admin1", geofence: "Las condes"),
-Admin.new(mail: "admin2@ad.com", password: "123", username: "admin2", geofence: "Lo barnechea"),
-Admin.new(mail: "admin3@ad.com", password: "123", username: "admin2", geofence: "Vitacura")]
+admins = [Admin.new(mail: "admin1@ad.com", password: "Qwer123", username: "admin11", geofence: "Las condes"),
+Admin.new(mail: "admin2@ad.com", password: "Qwer123", username: "admin2", geofence: "Lo barnechea"),
+Admin.new(mail: "admin3@ad.com", password: "Qwer123", username: "admin3", geofence: "Vitacura")]
 
 for a in admins do 
 	a.save!
 end
 
-sys = SystemAdmin.new(mail: "system@sy.com", username: "super1", password: "123")
+sys = SystemAdmin.new(mail: "system@sy.com", username: "super1", password: "Qwer123")
 
 sys.save!
 
