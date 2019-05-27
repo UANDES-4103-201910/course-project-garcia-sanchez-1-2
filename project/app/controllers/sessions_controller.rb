637a1654ea8d3002c73c87c55820e3c0911d03d7
resnakk <masanchez3@miuandes.cl>
2019-05-26 21:19:19 -0400

    hola

--- project/app/controllers/sessions_controller.rb
+++ project/app/controllers/sessions_controller.rb
@@ -21,8 +21,12 @@ class SessionsController < 	ApplicationController
 	end
 
 	def destroy
-		#complete this method
-		log_out
-		redirect_to root_path
+		@current_user = session[:current_user_id] = nil
+		session["warden.user.user.key"][0][0] = 0
+    	redirect_to root_url
 	end
+
+	def user_params
+      params.require(:session).permit(:email, :password)
+    end
 end
--- project/config/routes.rb
+++ project/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   
   devise_for :users
+  #devise_for :users, controllers: { sessions: 'users/sessions' }
+  #devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   get 'search/create'
   get 'search/new'
   get 'search/index'
@@ -17,11 +19,12 @@ Rails.application.routes.draw do
   resources :posts
   resources :profiles
   resources :registred_users
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'sessions#new'
+  root :to => 'posts#index'
 
   post '/login' => 'sessions#create', as: :log_in
-  get  '/log_out' => 'sessions#destroy', as: :log_out
+  
 
   post '/sign_up' => 'registration#create', as: :sign_up
   post '/new_post' => 'newest#create', as: :my_new_post
@@ -32,6 +35,8 @@ Rails.application.routes.draw do
   get '/report' => 'posts#report', as: :report
   
   get '/searched_users' => 'registred_users#search_user', as: :search_user
+
+  #delete '/users/sign_out' => 'users/sessions#destroy', as: :destroy_user_session
 
 
 end
