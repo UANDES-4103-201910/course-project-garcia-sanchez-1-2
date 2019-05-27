6a1fa1c53d6b7dc026adc0d8c104190f062e29e5
resnakk <masanchez3@miuandes.cl>
2019-05-26 20:19:35 -0400

    devise

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   
+  devise_for :users
   get 'search/create'
   get 'search/new'
   get 'search/index'
--- project/db/schema.rb
+++ project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_25_234732) do
+ActiveRecord::Schema.define(version: 2019_05_27_000731) do
 
   create_table "black_lists", force: :cascade do |t|
     t.integer "registred_user_id"
@@ -97,6 +97,27 @@ ActiveRecord::Schema.define(version: 2019_05_25_234732) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
