f75d03b3c698ca4c145e6977ebb020609bad3c47
resnakk <masanchez3@miuandes.cl>
2019-06-08 20:30:54 -0400

    wena

---
+++ project/db/migrate/20190609002755_remove_user_from_inappropiate.rb
@@ -0,0 +1,4 @@
+class RemoveUserFromInappropiate < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- project/db/schema.rb
+++ project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_020608) do
+ActiveRecord::Schema.define(version: 2019_06_09_002911) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
