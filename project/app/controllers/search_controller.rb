27ad46d458b2d9f847d01b275a1b10393b7ab0a8
Tomas <tmgarcia@miuandes.cl>
2019-06-09 04:36:26 -0400

    searchbar

--- project/app/controllers/search_controller.rb
+++ project/app/controllers/search_controller.rb
@@ -12,6 +12,9 @@ class SearchController < ApplicationController
       @users = User.where(["username || city || country LIKE ?", "%#{params[:search]}%"])
       #@citys = User.where(["city LIKE ?", "%#{params[:search]}%"])
       @posts = Post.where(["title || description LIKE ?", "%#{params[:search]}%"] )
+
+      @bl = User.where(["username LIKE ?", "%#{params[:search]}%"] && ["black_list LIKE ?", true])
+      @dump = Post.where(["title || description LIKE ?", "%#{params[:search]}%"] && ["dumpster LIKE ?", true])
     end   
   end
 end
